65681be08df13e8694fa7cad44dcfdbd892ede34
Brooklyn Abrams <158619377+brooklynabrams@users.noreply.github.com>
2025-09-22 15:04:21 -0400

    Skeleton for increment 1 closes #2

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>This is where you login</Text>
+    </View>
+  );
+}
+

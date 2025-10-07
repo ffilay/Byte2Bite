341c219ecc9ab9e410884ca62a88463ae6723741
carlycollins <90617293+carlycollins@users.noreply.github.com>
2025-10-06 20:34:49 -0400

    Functional CRUD for ingredients in frontend

--- frontend/app/components/IngredientTable.tsx
+++ frontend/app/components/IngredientTable.tsx
@@ -1,33 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState, useEffect } from 'react';
 import { BsTrash } from "react-icons/bs";
 import { ingredientsService, Ingredient } from '../../services/ingredientService'
 
-export default function IngredientTable() {
+type Props = {
+  ingredients: Ingredient[];
+  onEdit: (ingredient: Ingredient) => void;
+  onDelete: (id: number) => void;
+};
 
-    const [ingredients, setIngredients] = useState<Ingredient[]>([]);
-
-    // fetch all
-    const loadIngredients = async () => {
-        const data = await ingredientsService.getAllIngredients();
-        setIngredients(data);
-    };
-
-    // delete handler
-    const deleteIngredient = async (id: number) => {
-        try {
-        await ingredientsService.deleteIngredient(id);
-        console.log(`Deleted ingredient ${id}`);
-        // refresh list after deletion
-        setIngredients((prev) => prev.filter((ing) => ing.id !== id));
-        } catch (err) {
-        console.error("Error deleting ingredient:", err);
-        }
-    };
-
-    useEffect(() => {
-        loadIngredients();
-    }, []);
+export default function IngredientTable({ ingredients, onEdit, onDelete }: Props) {
 
     return (
     <div className="table-responsive">
@@ -42,6 +23,7 @@ export default function IngredientTable() {
             <th scope="col">Low Stock Threshold</th>
             <th scope="col">Max Stock</th>
             <th scope="col"></th>
+            <th scope="col"></th>
           </tr>
         </thead>
         <tbody>
@@ -54,7 +36,8 @@ export default function IngredientTable() {
               <td>{item.current_Stock}</td>
               <td>{item.low_Stock_Threshold}</td>
               <td>{item.max_Stock}</td>
-              <td><button className='btn btn-outline-primary' onClick={() => deleteIngredient(item.id)}><BsTrash/></button></td>
+              <td><button className='btn btn-outline-primary' onClick={() => onEdit(item)}>Edit</button></td>
+              <td><button className='btn btn-outline-primary' onClick={() => onDelete(item.id)}><BsTrash/></button></td>
             </tr>
           ))}
         </tbody>

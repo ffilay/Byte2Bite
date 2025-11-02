1d1f24bbbfe6474701f1876f47e46a8f1498030d
carlycollins <90617293+carlycollins@users.noreply.github.com>
2025-11-02 17:33:11 -0500

    import square catalog items

--- frontend/app/menuitems.tsx
+++ frontend/app/menuitems.tsx
@@ -7,13 +7,25 @@ export default function IngredientsPage() {
   const [Items, setItems] = useState<Items[]>([]);
 
   useEffect(() => {
-    const fetchMenuItems= async () => {
+    const importSquareCatalog = async () => {
+      try {
+        const data = await menuService.importMenuItems(
+          1 /*hardcoded to sandbox for now*/
+        );
+      } catch (err) {
+        console.error("Error importing square catalog:", err);
+      }
+    };
+    importSquareCatalog();
+  }, []);
+
+  useEffect(() => {
+    const fetchMenuItems = async () => {
       try {
         const data = await menuService.getAllMenuItems();
         console.log("Fetched menu items:", data);
         setItems(data);
-      }
-      catch (err){
+      } catch (err) {
         console.error("Error fetching menu items:", err);
       }
     };
@@ -23,9 +35,9 @@ export default function IngredientsPage() {
   console.log("Menu items:", Items);
 
   return (
-  <div className="container mt-4">
-  <h3 className="mt-4">Menu Inventory:</h3>
-      <MenuTable menuItem ={Items}/>
-  </div>
+    <div className="container mt-4">
+      <h3 className="mt-4">Menu Inventory:</h3>
+      <MenuTable menuItem={Items} />
+    </div>
   );
 }
--- frontend/services/menuService.ts
+++ frontend/services/menuService.ts
@@ -8,10 +8,29 @@ export interface Items {
   //restaurantID?
 }
 
-const API_URL = "http://localhost:5038/api/items";
+const API_URL = "http://localhost:5038/api";
+
+async function importMenuItems(
+  restaurantId: number
+): Promise<{ upserted: number }> {
+  const res = await fetch(
+    `${API_URL}/menu/import?restaurantId=${restaurantId}`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+
+  if (!res.ok) {
+    const error = await res.text();
+    throw new Error(error || "Failed to import menu items");
+  }
+
+  return res.json(); // returns { upserted: <number> }
+}
 
 async function getAllMenuItems(): Promise<Items[]> {
-  const res = await fetch(API_URL, {
+  const res = await fetch(`${API_URL}/items`, {
     method: "GET",
     headers: { "Content-Type": "application/json" },
   });
@@ -20,5 +39,6 @@ async function getAllMenuItems(): Promise<Items[]> {
 }
 
 export const menuService = {
+  importMenuItems,
   getAllMenuItems,
 };

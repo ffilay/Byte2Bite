6fc3f7ec16860dd6960ee2ab19f81320494417c6
Philip Quattrochi <philip.quattrochi@gmail.com>
2025-12-07 16:06:10 -0500

    menu item cost, profit, and count fix

--- backend/Mapping/AutoMapperProfile.cs
+++ backend/Mapping/AutoMapperProfile.cs
@@ -21,7 +21,12 @@ namespace backend.Mapping
             CreateMap<UserProfile, UserDto>().ReverseMap();
 
             // Map ItemDto -> Item
-            CreateMap<Item, ItemDto>().ReverseMap();
+            CreateMap<Item, ItemDto>()
+                .ForMember(dest => dest.TotalCost, opt => opt.MapFrom(src => src.Total_Cost ?? 0m))
+                .ForMember(dest => dest.ProfitMargin, opt => opt.MapFrom(src => src.Profit_Margin))
+                .ReverseMap()
+                .ForMember(dest => dest.Total_Cost, opt => opt.MapFrom(src => src.TotalCost ?? 0m))
+                .ForMember(dest => dest.Profit_Margin, opt => opt.MapFrom(src => src.ProfitMargin));
 
             // Map DistributorDto -> Distributor
             CreateMap<DistributorDto, Distributor>();
